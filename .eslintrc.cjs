72b448983439447bb006aaa26b61dc7471474de4
fkuca <fkuca91@gmail.com>
2023-10-04 12:08:39 +0200

    chore(eslint): add layout & formatting ESLint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,52 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+	],
+	ignorePatterns: ["dist", ".eslintrc.cjs"],
+	parser: "@typescript-eslint/parser",
+	plugins: ["react-refresh"],
+	rules: {
+		"react-refresh/only-export-components": [
+			"warn",
+			{ allowConstantExport: true },
+		],
+		"block-spacing": ["error", "always"],
+		"comma-style": ["error", "last"],
+		indent: ["error", "tab"],
+		"no-multiple-empty-lines": [
+			"error",
+			{
+				max: 1,
+			},
+		],
+		"max-len": [
+			"error",
+			{
+				code: 120,
+			},
+		],
+		"keyword-spacing": [
+			"error",
+			{
+				before: true,
+				after: true,
+			},
+		],
+		"no-trailing-spaces": ["error"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"@typescript-eslint/space-before-blocks": ["error"],
+		"@typescript-eslint/space-before-function-paren": [
+			"error",
+			{
+				anonymous: "always",
+				named: "never",
+				asyncArrow: "always",
+			},
+		],
+	},
+};
