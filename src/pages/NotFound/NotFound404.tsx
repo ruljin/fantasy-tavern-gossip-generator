53c93ef184fd58d0660b9fb9b1d8a04b34810c79
fkuca <fkuca91@gmail.com>
2023-12-06 20:56:29 +0100

    chore(pages): add dummy content to NotFound404 page

--- src/pages/NotFound/NotFound404.tsx
+++ src/pages/NotFound/NotFound404.tsx
@@ -1,8 +1,8 @@
 import { Card } from "../../components/card/Card";
 
 export const NotFound404 = () => (
-	<Card>
-		<Card.Body>NotFound404</Card.Body>
-		<Card.Footer>Buttons</Card.Footer>
-	</Card>
+  <Card>
+    <Card.Body>NotFound404</Card.Body>
+    <Card.Footer>Buttons</Card.Footer>
+  </Card>
 );
