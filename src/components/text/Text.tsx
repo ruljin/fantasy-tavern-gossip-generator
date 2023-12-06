85df66d02792e332c12dd2dd3079916a9e53730c
fkuca <fkuca91@gmail.com>
2023-12-06 20:51:23 +0100

    feat(text): add Text component

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -3,18 +3,18 @@ import { Typography } from "../../constants/Typography";
 import styles from "./Text.module.css";
 
 export interface TypographyProps {
-	children: string;
-	type?: Typography;
+  children: string;
+  type?: Typography;
 }
 
 export const Text = ({ children, type = "p" }: TypographyProps) => {
-	if (type === "h1") {
-		return (
-			<h1 className={`${styles.text} ${styles.text__header}`}>{children}</h1>
-		);
-	}
+  if (type === "h1") {
+    return (
+      <h1 className={`${styles.text} ${styles.text__header}`}>{children}</h1>
+    );
+  }
 
-	return (
-		<p className={`${styles.text} ${styles.text__paragraph}`}>{children}</p>
-	);
+  return (
+    <p className={`${styles.text} ${styles.text__paragraph}`}>{children}</p>
+  );
 };
