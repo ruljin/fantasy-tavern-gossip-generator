a7a7a0bede814ac779f6ebb77de9ac6d0b96ac13
fkuca <fkuca91@gmail.com>
2023-12-06 20:55:24 +0100

    feat(home): add Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,22 @@
 import { Card } from "../../components/card/Card";
+import { Text } from "../../components/text/Text";
+import { Button } from "../../components/button/Button";
 
 export const Home = () => (
-	<Card>
-		<Card.Body>Home</Card.Body>
-		<Card.Footer>Buttons</Card.Footer>
-	</Card>
+  <Card>
+    <Card.Body>
+      <Text type="h1">Hello traveler,</Text>
+      <Text>
+        First time with us? Hah! No wonder I don&apos;t know your face! What?
+        Would you like to know what is going on here in the region? Hah! No
+        problem, I&apos;ll be happy to tell you, of course ... for the
+        appropriate fee.
+      </Text>
+    </Card.Body>
+    <Card.Footer>
+      <Button isLink to="/generator">
+        Gossip
+      </Button>
+    </Card.Footer>
+  </Card>
 );
