4b0e046c2010a33cd03417ea31c3f8db4b81cf19
fkuca <fkuca91@gmail.com>
2023-12-06 20:58:02 +0100

    feat(routing): add routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ROUTES } from "./routes/Routes";
 
 export const App = () => (
-	<Page>
-		<BrowserRouter>
-			<Routes>
-				{ROUTES.map(({ path, element }) => (
-					<Route key={path} path={path} element={element} />
-				))}
-			</Routes>
-		</BrowserRouter>
-	</Page>
+  <Page>
+    <BrowserRouter>
+      <Routes>
+        {ROUTES.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  </Page>
 );
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,8 +2,9 @@ import type { ReactNode } from "react";
 import { Home } from "../pages/Home/Home";
 
 interface Route {
-	path: string;
-	element: ReactNode;
+  path: string;
+  element: ReactNode;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const ROUTES: Route[] = [{ path: "/", element: <Home /> }];
